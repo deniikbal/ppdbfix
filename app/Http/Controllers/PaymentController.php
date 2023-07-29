02c077295cf0b7c8608b7c5ac6e8e976f78e1bc9
deniikbal <deniikbal@gmail.com>
2023-07-29 15:27:00 +0700

    payment 1

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -83,43 +83,52 @@ class PaymentController extends Controller
         Config::$isSanitized = config('services.midtrans.isSanitized');
         Config::$is3ds = config('services.midtrans.is3ds');
 
-        $notification = new \Midtrans\Notification();
+        $notif = new \Midtrans\Notification();
 
-        // Buat instance midtrans notification
-        //$notification = new Notification();
-        dd($notification);
+        $transaction = $notif->transaction_status;
+        $type = $notif->payment_type;
+        $status_code = $notif->status_code;
+        $order_id = $notif->order_id;
+        $fraud = $notif->fraud_status;
+        $signature_key = $notif->signature_key;
+        $payment = Payment::where('order_id', $order_id)->first();
+        $signature = hash('sha512', $payment->order_id . 200 . $payment->gross_amount . env('MIDTRANS_SERVER_KEY'));
 
-        // Assign ke variable untuk memudahkan coding
-        $status = $notification->transaction_status;
-        $type = $notification->payment_type;
-        $fraud = $notification->fraud_status;
-        $order_id = $notification->order_id;
-
-        $transaction = Payment::findOrFail($order_id);
-
-        // Handle notification status midtrans
-        if ($status == 'capture') {
-            if ($type == 'credit_card') {
-                if ($fraud == 'challenge') {
-                    $transaction->status = 'PENDING';
-                } else {
-                    $transaction->status = 'SUCCESS';
-                }
-            }
-        } else if ($status == 'settlement') {
-            $transaction->status = 'SUCCESS';
-        } else if ($status == 'pending') {
-            $transaction->status = 'PENDING';
-        } else if ($status == 'deny') {
-            $transaction->status = 'CANCELLED';
-        } else if ($status == 'expire') {
-            $transaction->status = 'CANCELLED';
-        } else if ($status == 'cancel') {
-            $transaction->status = 'CANCELLED';
+        if ($signature != $signature_key) {
+            return abort(404);
         }
 
-        // Simpan transaksi
-        $transaction->save();
+        if ($transaction == 'capture') {
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card') {
+                if ($fraud == 'challenge') {
+                    // TODO set payment status in merchant's database to 'Challenge by FDS'
+                    // TODO merchant should decide whether this transaction is authorized or not in MAP
+                    echo "Transaction order_id: " . $order_id . " is challenged by FDS";
+                } else {
+                    // TODO set payment status in merchant's database to 'Success'
+                    echo "Transaction order_id: " . $order_id . " successfully captured using " . $type;
+                }
+            }
+        } else if ($transaction == 'settlement') {
+            // TODO set payment status in merchant's database to 'Settlement'
+            $payment = Payment::where('order_id', $order_id)->first();
+            $payment->update([
+                'transaction_status' => $transaction,
+            ]);
+        } else if ($transaction == 'pending') {
+            // TODO set payment status in merchant's database to 'Pending'
+            echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+        } else if ($transaction == 'deny') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+        } else if ($transaction == 'expire') {
+            // TODO set payment status in merchant's database to 'expire'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+        } else if ($transaction == 'cancel') {
+            // TODO set payment status in merchant's database to 'Denied'
+            echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+        }
     }
 
     /**
