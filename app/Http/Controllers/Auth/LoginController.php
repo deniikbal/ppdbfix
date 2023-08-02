2217033bcf9917d6c5705cdcd5503a7527344bee
deniikbal <deniikbal@gmail.com>
2023-08-02 09:00:34 +0700

    update login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::STUDENT;
 
     /**
      * Create a new controller instance.
