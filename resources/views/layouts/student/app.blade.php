91f7322b4787b3f6bdbdd3139359f277b71e6444
deniikbal <deniikbal@gmail.com>
2023-08-27 06:26:45 +0700

    update View

--- resources/views/layouts/student/app.blade.php
+++ resources/views/layouts/student/app.blade.php
@@ -31,7 +31,7 @@
     <meta name="author" content="ThemePixels">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="../../assets/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/assets/img/favicon.png')}}">
 
     <title>DashForge Responsive Bootstrap 4 Dashboard Template</title>
 
@@ -44,93 +44,92 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/2.0.0-alpha.2/cropper.css" rel="stylesheet">
     <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
     <script src="https://unpkg.com/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js"></script>
-    <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
+    <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet"/>
     <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
-        rel="stylesheet">
+          rel="stylesheet">
     <!-- DashForge CSS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/dashforge.css') }}">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css"
-        rel="stylesheet">
+          rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.css"
-        rel="stylesheet">
+          rel="stylesheet">
 </head>
 
 <body class="mn-ht-100v d-flex flex-column">
 
-    @include('layouts.student.header')
+@include('layouts.student.header')
 
-    <div class="content content-fixed">
-        <div class="container pd-x-0 pd-lg-x-10 pd-xl-x-0">
-            <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
-                <div>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb breadcrumb-style1 mg-b-10">
-                            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Sales Monitoring</li>
-                        </ol>
-                    </nav>
-                    <h4 class="mg-b-0 tx-spacing--1">Welcome to Dashboard, @if ($student == null)
-                            {{ auth()->user()->name }}
-                        @else
-                            {{ $student->name }}
-                        @endif
-                    </h4>
-                </div>
-                <div class="d-none d-md-block">
-                    {{--                <button class="btn btn-sm pd-x-15 btn-white btn-uppercase"><i data-feather="mail" class="wd-10 mg-r-5"></i> Email</button> --}}
-                    {{--                <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer" class="wd-10 mg-r-5"></i> Print</button> --}}
-                    <button class="btn btn-sm pd-x-15 btn-danger btn-uppercase mg-l-5"><i data-feather="clock"
-                            class="wd-10 mg-r-5"></i>
-                        {{ Carbon\carbon::now()->format('D, d M Y H:i:s') }}</button>
-                </div>
+<div class="content content-fixed">
+    <div class="container pd-x-0 pd-lg-x-10 pd-xl-x-0">
+        <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
+            <div>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb breadcrumb-style1 mg-b-10">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Sales Monitoring</li>
+                    </ol>
+                </nav>
+                <h4 class="mg-b-0 tx-spacing--1">Welcome to Dashboard, @if ($student == null)
+                        {{ auth()->user()->name }}
+                    @else
+                        {{ $student->name }}
+                    @endif
+                </h4>
             </div>
+            <div class="d-none d-md-block">
+                {{--                <button class="btn btn-sm pd-x-15 btn-white btn-uppercase"><i data-feather="mail" class="wd-10 mg-r-5"></i> Email</button> --}}
+                {{--                <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer" class="wd-10 mg-r-5"></i> Print</button> --}}
+                <button class="btn btn-sm pd-x-15 btn-danger btn-uppercase mg-l-5"><i data-feather="clock"
+                                                                                      class="wd-10 mg-r-5"></i>
+                    {{ Carbon\carbon::now()->format('D, d M Y H:i:s') }}</button>
+            </div>
+        </div>
 
-            <div class="row row-xs">
-                @yield('content')
-            </div><!-- row -->
-        </div><!-- container -->
-    </div><!-- content -->
+        <div class="row row-xs">
+            @yield('content')
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- content -->
 
-    @include('layouts.student.footer')
+@include('layouts.student.footer')
 
-    <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script src="{{ asset('/lib/parsleyjs/parsley.min.js') }}"></script>
-    <script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('lib/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('/lib/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('/lib/datatables.net-dt/js/dataTables.dataTables.min.js') }}"></script>
-    <script src="{{ asset('/lib/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('/lib/datatables.net-responsive-dt/js/responsive.dataTables.min.js') }}"></script>
-    <script src="{{ asset('lib/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('/lib/jquery-steps/build/jquery.steps.min.js') }}"></script>
-    <script src="https://unpkg.com/filepond-plugin-image-crop/dist/filepond-plugin-image-crop.js"></script>
-    <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
-    <script src="{{ asset('assets/js/dashforge.js') }}"></script>
-    @yield('script')
+<script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="{{ asset('/lib/parsleyjs/parsley.min.js') }}"></script>
+<script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('lib/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('/lib/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/lib/datatables.net-dt/js/dataTables.dataTables.min.js') }}"></script>
+<script src="{{ asset('/lib/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('/lib/datatables.net-responsive-dt/js/responsive.dataTables.min.js') }}"></script>
+<script src="{{ asset('lib/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('/lib/jquery-steps/build/jquery.steps.min.js') }}"></script>
+<script src="https://unpkg.com/filepond-plugin-image-crop/dist/filepond-plugin-image-crop.js"></script>
+<script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
+<script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+<script src="{{ asset('assets/js/dashforge.js') }}"></script>
+@yield('script')
 
-    <script>
-        //message with toastr
-        @if (session()->has('success'))
+<script>
+    //message with toastr
+    @if (session()->has('success'))
 
-            toastr.success('{{ session('success') }}', 'BERHASIL!');
-        @elseif (session()->has('error'))
-            toastr.error('{{ session('error') }}', 'GAGAL!');
-        @endif
-    </script>
+    toastr.success('{{ session('success') }}', 'BERHASIL!');
+    @elseif (session()->has('error'))
+    toastr.error('{{ session('error') }}', 'GAGAL!');
+    @endif
+</script>
 
-    <script>
-        $('.select2').select2({
-            placeholder: 'Pilih',
-            searchInputPlaceholder: 'Search options',
-        });
-    </script>
-
+<script>
+    $('.select2').select2({
+        placeholder: 'Pilih',
+        searchInputPlaceholder: 'Search options',
+    });
+</script>
 
 
 </body>
