dc5be9635c6f0a9d2231f927239fa25f2fe66e65
deniikbal <deniikbal@gmail.com>
2023-12-23 12:12:41 +0700

    updatesoftdelete

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,14 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $guarded = [];
-    protected $dates = ['deleted_at'];
 
     public function user(): BelongsTo
     {
