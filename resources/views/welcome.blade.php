0201d2d59681bd31204fc3af81df7b446158d870
deniikbal <deniikbal@gmail.com>
2024-08-23 16:57:23 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Register</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Daftar</h5>
                                 <button type="button" class="btn-close" data-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
